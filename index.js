0105009c2a62450235693d9fb74618a756d3a794
lodrick lawrence <lodricklowrence@gmail.com>
2024-06-18 13:57:27 +0300

    Solved task 2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // 1. Write a JavaScript function to calculate the sum of two numbers.
-const addNumbers = (a,b) => {
-    return a + b;
-}
+const addNumbers = (a, b) => {
+  return a + b;
+};
 
-console.log(addNumbers(10,5))
+//2. Write a JavaScript program to find the maximum number in an array.
+const arr = [1, 2, 3, 4, 5];
+const findMaxNumber = (arrValues) => {
+  return Math.max(...arrValues);
+};
+
+console.log(findMaxNumber(arr));
