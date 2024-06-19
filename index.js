62256d1a5491e78d3a6f562aca508e7695a761af
lodrick lawrence <lodricklowrence@gmail.com>
2024-06-19 14:53:46 +0300

    Revert "Solved task 5"
    This reverts commit 2c4aee49f6e2469efec83ade5861f08f9881c088.
    
    reverting the commit changes

--- index.js
+++ index.js
@@ -19,9 +19,4 @@ const reverseString = (str) => {
   return str.split("").reverse().join("");
 };
 
-//  5. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
-const evenNumbers = (arr) => {
-  return arr.filter((number) => number % 2 === 0);
-};
-
-console.log(evenNumbers([1,2,3,4,5,6]));
+console.log(reverseString("noo"));
